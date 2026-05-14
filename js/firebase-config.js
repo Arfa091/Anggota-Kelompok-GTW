a5302e887794dde441300983073953ba2254e302
unknown <Arfaship09@gmail.com>
2026-05-14 10:54:46 +0700

    carry nih

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -6,14 +6,14 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-aut
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCzBDdHWLMkWN7bH3oJBdri6s5KRBM6EHk",
-  authDomain: "monitoring-iot-29ac6.firebaseapp.com",
-  databaseURL: "https://monitoring-iot-29ac6-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "monitoring-iot-29ac6",
-  storageBucket: "monitoring-iot-29ac6.firebasestorage.app",
-  messagingSenderId: "4724063661",
-  appId: "1:4724063661:web:0a03e34b61072f9fd97524",
-  measurementId: "G-2Y44SR9LMN"
+  apiKey: "AIzaSyARWeAhj8vNXTeDwDe1a15hm-uzhPPwjC0",
+  authDomain: "monitoring-iot-6daae.firebaseapp.com",
+  databaseURL: "https://monitoring-iot-6daae-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "monitoring-iot-6daae",
+  storageBucket: "monitoring-iot-6daae.firebasestorage.app",
+  messagingSenderId: "557598750439",
+  appId: "1:557598750439:web:8d70d77ecabf46fdce5442",
+  measurementId: "G-DS2YLLDFHM"
 };
 
 const app = initializeApp(firebaseConfig);
